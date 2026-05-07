9c61883fefd2ae5ba189718353a0593f23b4945a
FXXKMaLife <lshine12393@gmail.com>
2026-05-07 12:38:59 +0900

    4강

--- IdeaProjects/p-14163-1/src/main/java/com/mysite/sbb/MainController.java
+++ IdeaProjects/p-14163-1/src/main/java/com/mysite/sbb/MainController.java
@@ -1,10 +1,19 @@
 package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
-    public void index() {
-        System.out.println("index() 메서드 호출됨!");
+    @GetMapping("/")
+    @ResponseBody
+    public String index() {
+        return "SBB";
+    }
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "hello";
     }
 }
